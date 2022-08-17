9f913f4ad52ca222ede3da652e092db90b280032
AlexisGH <89100320+alexisgohe@users.noreply.github.com>
2022-08-17 13:07:19 -0500

    Add files via upload

---
+++ Ejercicio4/main.js
@@ -0,0 +1,8 @@
+// Hacer un programa que muestre todos los números entre dos números introducidos por el usuario
+
+var num1 = 5
+var num2 = 15
+
+for (num1; num1 <= num2; num1++) {
+    console.log(num1)
+}
